475c9e41df40096590ddeaff6b779428d090dc21
RumiAust <rumi019aust@gmail.com>
2020-09-10 01:43:03 +0600

    Server port defined and dependency configured with test string in main class

--- src/main/java/aust/iums/pg/admission/PgAdmissionApplication.java
+++ src/main/java/aust/iums/pg/admission/PgAdmissionApplication.java
@@ -9,6 +9,7 @@ public class PgAdmissionApplication {
   //test
   public static void main(String[] args) {
     SpringApplication.run(PgAdmissionApplication.class, args);
+    System.out.println("Welcome to Pg Admission System");
   }
 
 }

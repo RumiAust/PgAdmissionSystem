54f43e5d4ee73f46afc79e624010122090486f37
RumiAust <rumi019aust@gmail.com>
2020-09-10 12:28:12 +0600

    Revert "Project directory Structure created"
    This reverts commit 301b3f3b

--- src/main/java/aust/iums/pg/admission/controller/AdmissionController.java
+++
@@ -1,7 +0,0 @@
-package aust.iums.pg.admission.controller;
-
-/**
- * Created by Rumi on 9/10/2020.
- */
-public class AdmissionController {
-}
--- src/main/java/aust/iums/pg/admission/helper/AdmissionHelper.java
+++
@@ -1,7 +0,0 @@
-package aust.iums.pg.admission.helper;
-
-/**
- * Created by Monjur-E-Morshed on 9/10/2020.
- */
-public class AdmissionHelper {
-}

cb3d901e950096dcfdebfdbf9434665fc472191c
RumiAust <rumi019aust@gmail.com>
2020-09-17 00:55:40 +0600

    AdmissionService created

---
+++ src/main/java/aust/iums/pg/admission/service/AdmissionService.java
@@ -0,0 +1,11 @@
+package aust.iums.pg.admission.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Monjur-E-Morshed on 9/17/2020.
+ */
+
+@Service
+public class AdmissionService {
+}

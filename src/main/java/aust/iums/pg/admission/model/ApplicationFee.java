f371e9d8d5ed60095b068f0435d5dcb43afcb5b0
monju <monjurmorshed793@gmail.com>
2020-10-04 20:24:02 +0600

    Application fee table configured with Project Lombok

---
+++ src/main/java/aust/iums/pg/admission/model/ApplicationFee.java
@@ -0,0 +1,24 @@
+package aust.iums.pg.admission.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="application_fee")
+@Getter @Setter @NoArgsConstructor @ToString
+public class ApplicationFee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(insertable = false, updatable = false)
+    private Semester semester;
+}

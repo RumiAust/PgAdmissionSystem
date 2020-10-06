6bd86879689fdf08c75d5d198ac522d0d4e6d5f0
monju <monjurmorshed793@gmail.com>
2020-10-06 14:06:23 +0600

    Jib maven plugin integrated for dockerizaiton

--- src/main/java/aust/iums/pg/admission/model/Payment.java
+++ src/main/java/aust/iums/pg/admission/model/Payment.java
@@ -2,15 +2,22 @@ package aust.iums.pg.admission.model;
 
 import aust.iums.pg.admission.enums.PaymentCategory;
 import aust.iums.pg.admission.enums.PaymentStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Objects;
 
 @Entity
 @Table(name="payment")
-public class Payment extends AbstractAuditingEntity {
+@Getter @Setter @NoArgsConstructor
+public class Payment extends AbstractAuditingEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
@@ -36,115 +43,4 @@ public class Payment extends AbstractAuditingEntity {
 
     private Instant paidOn;
 
-
-    public Payment() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public Instant getTransactionValidTill() {
-        return transactionValidTill;
-    }
-
-    public void setTransactionValidTill(Instant transactionValidTill) {
-        this.transactionValidTill = transactionValidTill;
-    }
-
-    public Applicant getApplicant() {
-        return applicant;
-    }
-
-    public void setApplicant(Applicant applicant) {
-        this.applicant = applicant;
-    }
-
-    public PaymentCategory getPaymentCategory() {
-        return paymentCategory;
-    }
-
-    public void setPaymentCategory(PaymentCategory paymentCategory) {
-        this.paymentCategory = paymentCategory;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public BigDecimal getWaivePercentage() {
-        return waivePercentage;
-    }
-
-    public void setWaivePercentage(BigDecimal waivePercentage) {
-        this.waivePercentage = waivePercentage;
-    }
-
-    public PaymentStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(PaymentStatus status) {
-        this.status = status;
-    }
-
-    public Instant getPaidOn() {
-        return paidOn;
-    }
-
-    public void setPaidOn(Instant paidOn) {
-        this.paidOn = paidOn;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Payment)) return false;
-        Payment payment = (Payment) o;
-        return Objects.equals(getId(), payment.getId()) &&
-                Objects.equals(getTransactionId(), payment.getTransactionId()) &&
-                Objects.equals(getTransactionValidTill(), payment.getTransactionValidTill()) &&
-                Objects.equals(getApplicant(), payment.getApplicant()) &&
-                getPaymentCategory() == payment.getPaymentCategory() &&
-                Objects.equals(getAmount(), payment.getAmount()) &&
-                Objects.equals(getWaivePercentage(), payment.getWaivePercentage()) &&
-                getStatus() == payment.getStatus() &&
-                Objects.equals(getPaidOn(), payment.getPaidOn());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getTransactionId(), getTransactionValidTill(), getApplicant(), getPaymentCategory(), getAmount(), getWaivePercentage(), getStatus(), getPaidOn());
-    }
-
-    @Override
-    public String toString() {
-        return "Payment{" +
-                "id=" + id +
-                ", transactionId='" + transactionId + '\'' +
-                ", transactionValidTill=" + transactionValidTill +
-                ", applicant=" + applicant +
-                ", paymentCategory=" + paymentCategory +
-                ", amount=" + amount +
-                ", waivePercentage=" + waivePercentage +
-                ", status=" + status +
-                ", paidOn=" + paidOn +
-                '}';
-    }
 }
